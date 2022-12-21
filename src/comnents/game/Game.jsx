8e6858fe8a7a2f9e0e94085b9053458d54983981
fariassilva <alessandrofarias405@gmail.com>
2022-12-21 07:22:01 -0300

    Trabalhando no gameoption

--- src/comnents/game/Game.jsx
+++ src/comnents/game/Game.jsx
@@ -1,22 +1,30 @@
+import { useState } from 'react'
 import styles from './Game.module.css'
 
 
 import GameOption from '../gameOption/GameOption'
 
+
+
+
 function Game(){
+  const [gameState , setGameState] = useState(Array(9).fill(0))
+
+  console.log(gameState)
+
 
   return(
 
     <div className={styles.game}>
-      <GameOption/>
-      <GameOption/>
-      <GameOption/>
-      <GameOption/>
-      <GameOption/>
-      <GameOption/>
-      <GameOption/>
-      <GameOption/>
-      <GameOption/>
+      {
+        gameState.map((value,pos) =>  
+        <GameOption
+        key = {`game-option-pos- ${pos}`}
+        status ={value}
+        />
+        )
+      }
+      
 
     </div>
 
